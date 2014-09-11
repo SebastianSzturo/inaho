9a1455b23efb850a7c791f570348e1fda9b5e929
Sebastian Szturo <s.szturo@me.com>
2014-09-11 18:35:06 +0900

    remove validation tests

--- spec/inaho/dictionary_spec.rb
+++ spec/inaho/dictionary_spec.rb
@@ -1,37 +1,34 @@
 require 'spec_helper'
 
 describe Inaho::Dictionary do
-  describe "#validate" do
-    it "should validate xml" do
+  describe "entries" do
+    it "should add entries and return array of entries" do
       dictionary = Inaho::Dictionary.new
-      entry = Inaho::Entry.new(id: "inaho", title: "稲穂", yomi: "いなほ")
-      entry.add_index("いなほ")
-      entry.add_index("inaho")
-      entry.body = "<h3>稲穂 (いなほ)</h3> <ul><li>ear (head) of rice</li></ul>"
-      dictionary << @entry
 
-      dictionary.validate.should == true
-    end
+      entry1 = Inaho::Entry.new(id: 1, title: "稲穂", yomi: "いなほ")
+      entry2 = Inaho::Entry.new(id: 2, title: "卵", yomi: "たまご")
 
-    it "should not be valid without index" do
-      dictionary = Inaho::Dictionary.new
-      entry = Inaho::Entry.new(id: "inaho", title: "稲穂", yomi: "いなほ")
-      entry.body = "<h3>稲穂 (いなほ)</h3> <ul><li>ear (head) of rice</li></ul>"
-      dictionary << @entry
+      dictionary << entry1
+      dictionary << entry2
 
-      dictionary.validate.should be_kind_of(Array)
-    end
-
-    it "should be valid without a yomi" do
-      dictionary = Inaho::Dictionary.new
-      entry = Inaho::Entry.new(id: "inaho", title: "稲穂")
-      entry.id = "inaho_1"
-      entry.add_index("いなほ")
-      entry.add_index("inaho")
-      entry.body = "<h3>稲穂 (いなほ)</h3> <ul><li>ear (head) of rice</li></ul>"
-
-      dictionary.validate.should == true
+      dictionary.entries.should be_kind_of(Array)
     end
   end
 
+  describe "#to_xml" do
+    it "should transform to xml" do
+      dictionary = Inaho::Dictionary.new
+
+      entry = Inaho::Entry.new(id: 1, title: "稲穂", yomi: "いなほ")
+      entry.add_index("稲穂")
+      entry.add_index("いなほ")
+      entry.add_index("inaho")
+      entry.add_index("ear (head) of rice")
+      entry.body = "<h3>稲穂 (いなほ)</h3> <ul><li>ear (head) of rice</li></ul>"
+
+      dictionary << entry
+
+      dictionary.to_xml.should_not be_nil
+    end
+  end
 end
